59cc78df77d66516c39adebd3a99c4742ebc3ad7
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-04-21 16:15:38 +0530

    PlayStatus also provides the PlayerControls

--- lib/src/video_player/builders/get_play_status.dart
+++ lib/src/video_player/builders/get_play_status.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:video_player/video_player.dart';
 
+import '../models/video_player_controls.dart';
 import '../providers/video_manager.dart';
 
 class GetUriPlayStatus extends ConsumerWidget {
@@ -11,28 +12,29 @@ class GetUriPlayStatus extends ConsumerWidget {
     super.key,
   });
   final Uri uri;
-  final Widget Function(
-    VideoPlayerValue? videoplayerStatus,
-  ) builder;
+  final Widget Function([
+    VideoPlayerControls? playerControls,
+    VideoPlayerValue? playStatus,
+  ]) builder;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final uriPlayControlAsync = ref.watch(videoPlayerProvider);
-
+    final playerControls = ref.watch(videoPlayerProvider.notifier);
     return uriPlayControlAsync.when(
       data: (uriPlayControl) {
         if (uriPlayControl.controller == null || uriPlayControl.path != uri) {
-          return builder(null);
+          return builder();
         }
         return ValueListenableBuilder(
           valueListenable: uriPlayControl.controller!,
-          builder: (context, value, child) {
-            return builder(value);
+          builder: (context, playStatus, child) {
+            return builder(playerControls, playStatus);
           },
         );
       },
-      error: (_, __) => builder(null),
-      loading: () => builder(null),
+      error: (_, __) => builder(),
+      loading: builder,
     );
   }
 }
