f3b2d4064e901fb162705b3e4bd89a6618ea5fd5
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-04-23 11:47:12 +0530

    add documetnation  in cl_media_viewers_flutter.dart

--- lib/cl_media_viewers_flutter.dart
+++ lib/cl_media_viewers_flutter.dart
@@ -1,33 +1,49 @@
 /// Media Viewers
 library;
 
-export 'src/basics/models/utils.dart'
-    show doubleToDuration, durationToDouble; // Remove!!
+import 'package:cl_media_viewers_flutter/src/image_viewer/image_viewer.dart';
+import 'package:cl_media_viewers_flutter/src/image_viewer/overlay_widget.dart';
+import 'package:cl_media_viewers_flutter/src/media_viewer.dart';
+import 'package:cl_media_viewers_flutter/src/video_player/builders/get_uri_play_status.dart';
+import 'package:cl_media_viewers_flutter/src/video_player/builders/get_video_player_controls.dart';
+import 'package:cl_media_viewers_flutter/src/video_player/views/media_fullscreen_view.dart';
+import 'package:cl_media_viewers_flutter/src/video_player/views/video_player.dart';
+
+/// Implements Widgets required to play Media.
+///   Currently supports Image and Video
+///   [VideoPlayer], [ImageViewer] and [MediaViewer] for raw viewing.
+///
+/// To show preview with overlay icons, or some minimal controls,
+///   use [ImageViewer] and [OverlayWidgets]
+///
+/// A default control [MediaPlayerControls] is implemented with the following features
+///   For Image, rotateview
+///   For Video, play/pause Audio Mute, FullScreen toggle, rotateview,
+///       Play Slider and time display
+///   Note, [MediaPlayerControls] don't create [MediaViewer] itself and
+///     you need to create and pass it as a widget. This is to avoid
+///     rebuild of the core player that will create artifacts  / flicker
+///
+///   If you prefer to create your control, you can use
+///     [GetVideoPlayerControls] to get access to all the available controls
+///     exposed via [MediaPlayerControls] and
+///     [GetUriPlayStatus] to get the status of the player
+///
+///
+
 export 'src/config/builders/get_fullscreen_status.dart'
     show GetFullScreenStatus;
-
 export 'src/config/models/media_view_modifier.dart' show MediaViewModifier;
 export 'src/image_viewer/image_viewer.dart'
     show ImageViewer; // can we pass this over MediaViewer?
 export 'src/image_viewer/overlay_widget.dart' show OverlayWidgets;
 export 'src/media_viewer.dart' show MediaViewer;
+export 'src/video_player/builders/get_uri_play_status.dart'
+    show GetUriPlayStatus;
 export 'src/video_player/builders/get_video_player_controls.dart'
     show GetVideoPlayerControls;
 export 'src/video_player/models/video_player_controls.dart'
     show VideoPlayerControls;
 export 'src/video_player/views/media_fullscreen_view.dart'
     show MediaPlayerControls;
-
 export 'src/video_player/views/video_player.dart' show VideoPlayer;
-
-/*
-export 'src/config/models/persist_json.dart' show PersistJson;
-
-/* export 'src/config/builders/get_media_view_modifier.dart'
-    show GetMediaViewModifier; */
-
-
-export 'src/video_player/builders/get_uri_play_status.dart'
-    show GetUriPlayStatus;
-
- */
